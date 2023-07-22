97af279131099cb4949dd5c5131427c5462e0680
Yeap-Chun-Hong <chunhong2002@outlook.com>
2023-07-22 16:24:58 +0800

    set up function prototype

--- UCCD1024-Assigment1/app.cpp
+++ UCCD1024-Assigment1/app.cpp
@@ -1,84 +1,34 @@
-#include<iostream>
-#include<iomanip>
+#include	<iostream>
+#include	<cstdlib>
+#include	<cstdio>
+#include    <fstream>
+#include    <string>
+#include	"List.h"
+#include    "LibStudent.h"
+#include    "LibBook.h"
+
 
 using namespace std;
 
-void MainMenu();
-void center(string);
+bool ReadFile(string, List*);
+bool DeleteRecord(List*, char*);
+bool Display(List, int, int);
+bool InsertBook(string, List*);
+bool SearchStudent(List*, char* id, LibStudent&);
+bool computeAndDisplayStatistics(List*);
+bool printStuWithSameBook(List*, char*);
+bool displayWarnedStudent(List*, List*, List*);
+int menu();
+
 
 int main() {
-	MainMenu();
 
 
-
+	cout << "\n\n";
+	system("pause");
 	return 0;
 }
-void center(string string) {
-	const int WIDTH = 115;
-	int space = (WIDTH - string.length()) / 2;
-	cout << setw(space + string.length()) << string << endl;
-}
 
-void MainMenu() {
 
-	bool stop = false;
-	do {
-		system("cls");
-		int operation;
-		center("Faculty of Information and Communication Technology");
 
-		center("UTAR Library System");
-		center("Main Menu");
-		center("Please select an option:          ");
-		center("1. Read File                      ");
-		center("2. Delete Record                  ");
-		center("3. Search Student                 ");
-		center("4. Insert Book                    ");
-		center("5. Display Output                 ");
-		center("6. Compute and Display Statistics ");
-		center("7. Student with Same Book         ");
-		center("8. Display Warned Student         ");
-		center("9. Exit                           ");
-		cout << "Enter an operation: ";
-		cin >> operation;
 
-		switch (operation) {
-		case 1:
-
-			break;
-		case 2:
-
-			break;
-		case 3:
-
-			break;
-		case 4:
-
-			break;
-		case 5:
-
-			break;
-
-		case 6:
-
-			break;
-		case 7:
-
-			break;
-		case 8:
-
-			break;
-		case 9:
-			stop = true;
-			break;
-		default:
-			cout << "Invalid Input!\a" << endl;
-			system("pause");
-
-		}
-		cin.clear();
-		cin.ignore();
-	} while (!stop);
-
-	return;
-}
