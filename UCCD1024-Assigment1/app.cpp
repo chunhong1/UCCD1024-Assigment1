0716d127a3b7b8d2ee6f59e001e083f55c6c66e8
Yeap-Chun-Hong <chunhong2002@outlook.com>
2023-07-23 17:43:18 +0800

    complete bool ReadFile function

--- UCCD1024-Assigment1/app.cpp
+++ UCCD1024-Assigment1/app.cpp
@@ -7,10 +7,14 @@
 #include    "LibStudent.h"
 #include    "LibBook.h"
 
-
 using namespace std;
 
+//constant
+const string STUDENT_INFO_FILE = "student.txt";
+
+//function prototype
 bool ReadFile(string, List*);
+bool isDuplicate(List* list, LibStudent& student);
 bool DeleteRecord(List*, char*);
 bool Display(List, int, int);
 bool InsertBook(string, List*);
@@ -22,13 +26,97 @@ int menu();
 
 
 int main() {
+    List studentList; // Create an instance of the List class
 
+    if (ReadFile(STUDENT_INFO_FILE, &studentList)) {
+        cout << "Data successfully read and stored in the list." << endl;
+        cout << studentList.count;
+    }
+    else {
+        cout << "Failed to read data or encountered an error." << endl;
+    }
 
 	cout << "\n\n";
 	system("pause");
 	return 0;
 }
 
+// Read student information from the file "student.txt" and store it in the linked list
+bool ReadFile(string filename, List* list) {
+    ifstream in;
+    char text[256];
 
+    LibStudent newStudent;
 
+    in.open(filename);
 
+    //return false if the file is not found
+    if (!in)
+    {
+        return false;
+    }
+    
+    while (!in.eof())
+    {
+        //ignore "Student Id ="
+        for (int j = 0; j < 3; j++)
+            in >> text;
+
+        //assign student id
+        in >> newStudent.id;
+
+        //ignore "Name ="
+        for (int j = 0; j < 2; j++)
+            in >> text;
+
+        //assign student name
+        in >> newStudent.name;
+        in.getline(text, 256);
+        strcat_s(newStudent.name, text);
+
+        //ignore "course ="
+        for (int j = 0; j < 2; j++)
+            in >> text;
+
+        //assign course
+        in >> newStudent.course;
+
+        //ignore "Phone Number ="
+        for (int j = 0; j < 3; j++)
+            in >> text;
+
+        //assign phone number
+        in >> newStudent.phone_no;
+
+        // Check if the student is a duplicate, if not then insert student into the list
+        if (!isDuplicate(list, newStudent)) 
+        {
+            list->insert(newStudent);
+        }
+    }
+
+    in.close();
+    return true;
+}
+
+// function to check if a student record is a duplicate in the list
+bool isDuplicate(List* list, LibStudent& student) { 
+    Node* cur;
+
+    //list will not duplicate if it is empty
+    if (list->empty())
+    {
+        return false;
+    }
+
+    cur = list->head; //start traversing from the first node
+    
+    while (cur != NULL)
+    {
+        if (cur->item.compareName2(student))
+            return true;
+        else
+            cur = cur->next; //move to next node in the list
+    }
+    return false;
+}
